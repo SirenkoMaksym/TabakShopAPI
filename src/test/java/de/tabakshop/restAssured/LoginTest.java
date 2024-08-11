de36b8fb36a8d0532cdf5f39c80f45098415c99d
SirenkoMaksym <157710113+SirenkoMaksym@users.noreply.github.com>
2024-08-11 22:23:38 +0200

    Update LoginTest.java

--- src/test/java/de/tabakshop/restAssured/LoginTest.java
+++ src/test/java/de/tabakshop/restAssured/LoginTest.java
@@ -13,41 +13,4 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class LoginTest extends TestBase{
-    AuthRequestDto requestDto = AuthRequestDto.builder()
-            .email("test34@gmail.com")
-            .password("7465HHHY***$64")
-            .build();
-
-    @Test
-    public void loginSuccessTest() {
-        AuthResponseDto dto = given()
-                .contentType("application/json")
-                .body(requestDto)
-                .post("login")
-                .then()
-                .assertThat().statusCode(200)
-                .extract().response().as(AuthResponseDto.class);
-
-        Assert.assertTrue(dto.getAccessToken() != null);
-    }
-//    @Test
-//    public void loginWrongPasswordPerfectTest(){
-//        given()
-//                .body(AuthRequestDto.builder()
-//                        .username("maxtest@gmail.com")
-//                        .password("Maxtest1234!").build())
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .post("user/login/usernamepassword")
-//                .then()
-//                .assertThat().statusCode(401)
-//                .assertThat().body("error", equalTo("Unauthorized"));
-//
-//    }
-
-
-
-
-
-
 }
